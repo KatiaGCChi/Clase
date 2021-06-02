f0a7e7dbb17e7aed8d2f9c6430d63dce4b8a3a19
WALMART4139 <WALMART4139@LAPTOP-IHJQ63LQ>
2021-06-01 20:37:48 -0500

    clase

---
+++ src/clase/Clase.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase;
+import java.util.Scanner;
+/**
+ *
+ * @author Katia 
+ */
+public class Clase {
+    
+        /**
+         * @param args the command line arguments
+         */
+   
+        private String Nro_Hc;
+        private String Dri;
+        private String Domicilia;
+        private String Nombre;
+        
+                
+
+    public String getNro_Hc() {
+        return Nro_Hc;
+    }
+    
+    public void setNro_Hc(String Nro_Hc) {
+        this.Nro_Hc = Nro_Hc;
+    }
+
+    public String getDri() {
+        return Dri;
+    }
+
+    public void setDri(String Dri) {
+        this.Dri = Dri;
+    }
+
+    public String getDomicilia() {
+        return Domicilia;
+    }
+
+    public void setDomicilia(String Domicilia) {
+        this.Domicilia = Domicilia;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+    }
+    
+
